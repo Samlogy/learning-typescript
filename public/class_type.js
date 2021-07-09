3ada440ac7d374e1bdd331320a3ea0d319e8380c
Samlogy <senanisammy@gmail.com>
2021-07-09 09:45:05 +0200

    class polymorphism + inheritance ...

--- public/class_type.js
+++ public/class_type.js
@@ -3,6 +3,7 @@ class Person {
     // public: accessible anywhere else, 
     // private: inside class only, 
     // readonly: anywhere else but can't be changed
+    // protected: inside the class & it children
     constructor(n, a, e, z) {
         this.name = n;
         this.age = a;
@@ -11,6 +12,12 @@ class Person {
     }
     greeting() {
         return `Hello my name is ${this.name} and i'm ${this.age} years old, my email address: ${this.email}, my current salary is: ${this.salary}`;
+    }
+    greet() {
+        return "hello i'm parent class !";
+    }
+    getName() {
+        return this.name;
     }
 }
 const p1 = new Person('Sam', 25, 'sam@gmail.com', 50000);
@@ -24,3 +31,21 @@ for (let person of persons) {
     console.log(person.greeting());
 }
 // interfaces can be used on obj, classes, 
+// inheritance
+class Developer extends Person {
+    greet() {
+        return "hello i'm child class !";
+    }
+    greetWithParentMethod() {
+        super.greet();
+    }
+    greetWithChildMethod() {
+        this.greet();
+    }
+}
+const dev1 = new Developer('Sammy', 25, 'sam@gmail.com', 50000);
+console.log(dev1.greeting());
+console.log(dev1.getName());
+console.log(dev1.greet());
+console.log(dev1.greetWithParentMethod());
+console.log(dev1.greetWithChildMethod());
--- src/class_type.ts
+++ src/class_type.ts
@@ -3,6 +3,8 @@ interface IPerson {
     age: number;
     email: string;
     salary: number;
+    greeting(): void;
+    getName(): string;
 }
 
 class Person implements IPerson {
@@ -14,6 +16,7 @@ class Person implements IPerson {
     // public: accessible anywhere else, 
     // private: inside class only, 
     // readonly: anywhere else but can't be changed
+    // protected: inside the class & it children
 
     constructor(n: string, a:number, e: string, z: number) {
         this.name = n;
@@ -24,6 +27,12 @@ class Person implements IPerson {
 
     greeting() {
         return `Hello my name is ${this.name} and i'm ${this.age} years old, my email address: ${this.email}, my current salary is: ${this.salary}`
+    }
+    greet() {
+        return "hello i'm parent class !"
+    }
+    getName() {
+        return this.name
     }
 }
 const p1 = new Person('Sam', 25, 'sam@gmail.com', 50000)
@@ -38,3 +47,24 @@ for (let person of persons) {
     console.log(person.greeting())
 }
 // interfaces can be used on obj, classes, 
+
+// inheritance
+class Developer extends Person {
+
+    greet() {
+        return "hello i'm child class !"
+    }
+    greetWithParentMethod() {
+        super.greet()
+    }
+    greetWithChildMethod() {
+        this.greet()
+    }
+}
+const dev1 = new Developer('Sammy', 25, 'sam@gmail.com', 50000)
+
+console.log(dev1.greeting())
+console.log(dev1.getName())
+console.log(dev1.greet())
+console.log(dev1.greetWithParentMethod())
+console.log(dev1.greetWithChildMethod())
